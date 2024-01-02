5a0d31878aea65b99242977f3ee1ec26436d56a4
pagmerek <p.gmerek7@gmail.com>
2024-01-02 22:38:41 +0100

    add nvim lazy config

---
+++ nvim.lazy/lua/config/options.lua
@@ -0,0 +1,41 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.mapleader = " "
+
+vim.opt.mouse = "a"
+vim.opt.nu = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- appearance
+vim.opt.termguicolors = true
+vim.opt.cmdheight = 1
+vim.opt.updatetime = 5
+vim.opt.scrolloff = 8
+
+-- other
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.backspace = "indent,eol,start"
+
+-- netrw
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = -25
